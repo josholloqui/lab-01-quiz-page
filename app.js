a95e438a4cd54ef0712dee5e9225b130323f2fe4
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-07-06 19:29:06 -0700

    this commits add the ability to cancel the quiz when asked to confirm

--- app.js
+++ app.js
@@ -4,5 +4,16 @@
  const quizButton = document.getElementById('quiz-button');
 // set event listeners to update state and DOM
 quizButton.addEventListener('click', () => {
-    console.log('button')
+    //ask for name
+    const firstName = prompt('What is your first name?');
+    //confirming that they want to take quiz
+    const areYouSure = confirm('Are you sure you are ready?');
+    //cancels start of quiz
+    if (!really) return;
+
+    const paws = prompt('Are my paws often referred to "Grinch Feet"?')
+
+    const cuddling = prompt('Do I hate cuddling?');
+
+    const kokoGF =prompt('Do I consider KoKo my girlfriend?')
 })
