5f0752bf9ba6194ec01bc460f6ec61df3f22717e
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-07-07 00:14:17 -0700

    fixed some syntax errors

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
 // import functions and grab DOM elements
-import { translatesToYes } from './yesfunction.js' 
+import { translatesToYes } from './yesfunction.js';
 // grabbing button from html
 const quizButton = document.getElementById('quiz-button');
 //grabbing the span to print results of quiz
 const quizResults = document.getElementById('results');
-console.log(quizResults)
 // set event listeners to update state and DOM
 quizButton.addEventListener('click', () => {
     //ask for name
     const firstName = prompt('What is your first name?');
-    console.log(firstName);
     //confirming that they want to take quiz
     const areYouSure = confirm('Are you sure you are ready?');
     //cancels start of quiz
-    if (!areYouSure) {
-        return
-    }
+    if (!areYouSure) return;
     //answer is yes
-    const answer1 = prompt('Are my paws often referred to "Grinch Feet"?')
-    console.log(answer1);
+    const answer1 = prompt('Are my paws often referred to "Grinch Feet"?');
     //answer is no
     const answer2 = prompt('Do I hate cuddling?');
-    console.log(answer2);
     //answer is yes
-    const answer3 = prompt('Do I consider KoKo my girlfriend?')
-    console.log(answer3);
+    const answer3 = prompt('Do I consider KoKo my girlfriend?');
     // the score starts at zero
     let score = 0;
     //adds to score if first question returns true(yes)
@@ -39,9 +32,8 @@ quizButton.addEventListener('click', () => {
     if (translatesToYes(answer3)) {
         score++;
     }
-    console.log(score);
     //alerts user that the quiz is over and results to be posted on page
-    alert('The quiz is completed! Results will be posted on the page!')
+    alert('The quiz is completed! Results will be posted on the page!');
     // results get printed in the span element in html
     quizResults.textContent = `Congratulations ${firstName} you have completed the quiz! Let us see how you did... You got ${score} out of 3.`;
 });
