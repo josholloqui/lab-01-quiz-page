d0936732fd0dd1944febc1f578269b6d1b25be9a
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-07-06 23:13:25 -0700

    this commit finaaalllly fixes my span issue

--- app.js
+++ app.js
@@ -1,39 +1,47 @@
 // import functions and grab DOM elements
-import { translatesToYes } from './yes-function.js' 
+import { translatesToYes } from './yesfunction.js' 
 // grabbing button from html
- const quizButton = document.getElementById('quiz-button');
+const quizButton = document.getElementById('quiz-button');
 //grabbing the span to print results of quiz
-const quizResults = document.getElementById('quiz-results');
+const quizResults = document.getElementById('results');
+console.log(quizResults)
 // set event listeners to update state and DOM
 quizButton.addEventListener('click', () => {
     //ask for name
     const firstName = prompt('What is your first name?');
+    console.log(firstName);
     //confirming that they want to take quiz
     const areYouSure = confirm('Are you sure you are ready?');
     //cancels start of quiz
-    if (!really) return;
+    if (!areYouSure) {
+        return
+    }
     //answer is yes
-    const paws = prompt('Are my paws often referred to "Grinch Feet"?')
+    const answer1 = prompt('Are my paws often referred to "Grinch Feet"?')
+    console.log(answer1);
     //answer is no
-    const cuddling = prompt('Do I hate cuddling?');
+    const answer2 = prompt('Do I hate cuddling?');
+    console.log(answer2);
     //answer is yes
-    const kokoGF =prompt('Do I consider KoKo my girlfriend?')
-    //alerts user that the quiz is over and results to be posted on page
-    const alertsUser = alert('The quiz is completed! Results will be posted on the page!')
+    const answer3 = prompt('Do I consider KoKo my girlfriend?')
+    console.log(answer3);
+    // the score starts at zero
     let score = 0;
-
     //adds to score if first question returns true(yes)
-    if (translatesToYes(paws)) {
-        return score++;
-    };
+    if (translatesToYes(answer1)) {
+        score++;
+    }
     //adds to score if second question returns false(no)
-    if (!translatesToYes(cuddling)) {
-        return score++;
-    };
+    if (!translatesToYes(answer2)) {
+        score++;
+    }
     //adds to score if third question returns true(yes)
-    if (translatesToYes(kokoGF)) {
-        return score++;
-    };
-
-
-})
+    if (translatesToYes(answer3)) {
+        score++;
+    }
+    console.log(score);
+    //alerts user that the quiz is over and results to be posted on page
+    alert('The quiz is completed! Results will be posted on the page!')
+    // results get printed in the span element in html
+    quizResults.textContent = 'hello please throw me a bone';
+});
