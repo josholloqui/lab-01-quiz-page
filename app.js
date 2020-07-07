d4f6861283aa575fa0149fc6fbe4c92920e95f04
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-07-06 19:52:56 -0700

    this commits adds alert that the quiz is finished

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // import functions and grab DOM elements
 import { translatesToYes } from './yes-function.js' 
-// initialize state
+// grabbing button from html
  const quizButton = document.getElementById('quiz-button');
+//grabbing the span to print results of quiz
+const quizResults = document.getElementById('quiz-results');
 // set event listeners to update state and DOM
 quizButton.addEventListener('click', () => {
     //ask for name
@@ -16,6 +18,22 @@ quizButton.addEventListener('click', () => {
     const cuddling = prompt('Do I hate cuddling?');
     //answer is yes
     const kokoGF =prompt('Do I consider KoKo my girlfriend?')
-
+    //alerts user that the quiz is over and results to be posted on page
+    const alertsUser = alert('The quiz is completed! Results will be posted on the page!')
     let score = 0;
+
+    //adds to score if first question returns true(yes)
+    if (translatesToYes(paws)) {
+        return score++;
+    };
+    //adds to score if second question returns false(no)
+    if (!translatesToYes(cuddling)) {
+        return score++;
+    };
+    //adds to score if third question returns true(yes)
+    if (translatesToYes(kokoGF)) {
+        return score++;
+    };
+
+
 })
