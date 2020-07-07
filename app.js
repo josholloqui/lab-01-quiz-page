91328c430938cba78ca755bea4d37f07ac9fa0a5
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-07-06 18:47:26 -0700

    button functionality

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // import functions and grab DOM elements
 
 // initialize state
-
+ const quizButton = document.getElementById('quiz-button');
 // set event listeners to update state and DOM
+quizButton.addEventListener('click', () => {
+    console.log('button')
+})
